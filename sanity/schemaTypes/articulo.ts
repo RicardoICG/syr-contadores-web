d4c71a98001381427880fbcb6ff130949c1f973a
Ricardo Cervantes <ricardoiscervantesg@gmail.com>
2026-09-01 10:46:05 -0500

    Agregado el molde de Artículos del Blog a Sanity

---
+++ sanity/schemaTypes/articulo.ts
@@ -0,0 +1,38 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'articulo',
+  title: 'Artículos del Blog',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'titulo',
+      title: 'Título del Artículo',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'URL del Artículo (Slug)',
+      description: 'Haz clic en "Generate" para crear la URL automáticamente basada en el título.',
+      type: 'slug',
+      options: { source: 'titulo' }
+    }),
+    defineField({
+      name: 'fecha',
+      title: 'Fecha de publicación',
+      type: 'date',
+    }),
+    defineField({
+      name: 'imagen',
+      title: 'Imagen Principal',
+      type: 'image',
+      options: { hotspot: true }
+    }),
+    defineField({
+      name: 'contenido',
+      title: 'Contenido del Artículo',
+      type: 'array',
+      of: [{ type: 'block' }]
+    }),
+  ],
+})
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import recurso from './recurso'
+import articulo from './articulo'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [recurso],
+  types: [recurso, articulo],
 }
